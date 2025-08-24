741fc43e184ae9c01b64aec5e9cc116842c4dd1d
Shahariarshehab <mdshahariaislam11@gmail.com>
2025-08-24 20:02:23 +0600

    shehab

---
+++ Introduction to Algorithm/Module-02/adjacancy_list_for_undirected_graph.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    vector<int>v(n);
+    while(e--)
+    {
+        int a,b;
+        cin>>a>>b;
+        v.push_back(a);
+    }
+    return 0;
+}
