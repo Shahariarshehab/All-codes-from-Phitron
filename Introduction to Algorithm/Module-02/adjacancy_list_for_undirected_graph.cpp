96d8a25868874b6365815fe54340aec009509734
Shahariarshehab <mdshahariaislam11@gmail.com>
2025-08-27 20:59:32 +0600

    shehab

--- Introduction to Algorithm/Module-02/adjacancy_list_for_undirected_graph.cpp
+++ Introduction to Algorithm/Module-02/adjacancy_list_for_undirected_graph.cpp
@@ -1,15 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n,e;
-    cin>>n>>e;
-    vector<int>v(n);
-    while(e--)
+    int n, e;
+    cin >> n >> e;
+    vector<int> adj_list[n];
+    while (e--)
     {
-        int a,b;
-        cin>>a>>b;
-        v.push_back(a);
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << "->";
+        for (int x : adj_list[i])
+        {
+            cout << x << " ";
+        }
+        cout << "\n";
     }
     return 0;
 }
