821b50dfe102187ab1e2a1af9c96d13a6e26b2fd
Shahariarshehab <mdshahariaislam11@gmail.com>
2025-11-24 21:28:29 +0600

    shehab

---
+++ XPSC-Problem Solving Club/Weekly Problems/Week-06/S_To_Divide_or_Not_To_Divide.cpp
@@ -0,0 +1,54 @@
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+// "Verily, with hardship comes ease." – [Qur'an 94:6]
+// Author: Md.Shaharia Islam
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ff first
+#define ss second
+#define nl '\n'
+#define HA cout << "YES" << nl
+#define NA cout << "NO" << nl
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Shaharia_ar_solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, n;
+        cin >> a >> b >> n;
+        if (a % b==0)
+        {
+            cout << -1 << nl;
+            continue;
+        }
+
+        ll d = ((n + a - 1) / a) * a;
+        while (d % b == 0)
+        {
+            d += a;
+        }
+        cout << d << nl;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Shaharia_ar_solve();
+
+    return 0;
+}
