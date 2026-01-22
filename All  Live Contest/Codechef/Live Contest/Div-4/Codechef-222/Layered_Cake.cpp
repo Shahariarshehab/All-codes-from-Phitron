2c8a076fd5788f21884ab6d08af87997b0a0fe2b
Shahariarshehab <mdshahariaislam11@gmail.com>
2026-01-22 20:05:36 +0600

    shehab

---
+++ All  Live Contest/Codechef/Live Contest/Div-4/Codechef-222/Layered_Cake.cpp
@@ -0,0 +1,63 @@
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+// "Verily, with hardship comes ease." – [Qur'an 94:6]
+// Author: Md.Shaharia Islam
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ff first
+#define ss second
+#define nl '\n'
+#define HA cout << "YES" << nl
+#define NA cout << "NO" << nl
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Shaharia_ar_solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vi a(n), b(m);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> b[i];
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (b[j] < a[i])
+                {
+                    cnt++;
+                }
+            }
+        }
+        cout << cnt << nl;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Shaharia_ar_solve();
+
+    return 0;
+}
---
+++ XPSC-Problem Solving Club/Newbie Problem Solvers/Weekly Problems/Week-10 (Random Problem)/E_Jump_A_or_B.cpp
@@ -0,0 +1,51 @@
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+// "Verily, with hardship comes ease." – [Qur'an 94:6]
+// Author: Md.Shaharia Islam
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ff first
+#define ss second
+#define nl '\n'
+#define HA cout << "YES" << nl
+#define NA cout << "NO" << nl
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Shaharia_ar_solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, a, b;
+        cin >> n >> m >> a >> b;
+        int c = (m - (b * n)) / (a - b);
+        if ((m - (b * n)) % (a - b) == 0 && (c >= 0 && c <= n))
+        {
+            cout << "Yes" << nl;
+        }
+        else
+        {
+            cout << "No" << nl;
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Shaharia_ar_solve();
+
+    return 0;
+}
